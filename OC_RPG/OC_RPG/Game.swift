b6bd2e77f5df62323d19a16bebe00c39c3d1cab1
kcourtois <courtois.k2@gmail.com>
2018-12-23 16:18:48 +0100

    Moved players to playermanager. Will have to rethink the way i use it

--- OC_RPG/OC_RPG/Game.swift
+++ OC_RPG/OC_RPG/Game.swift
@@ -11,54 +11,55 @@ import Foundation
 //This class will hold all the datas of a game.
 //Everything that happens in the game will be handled here.
 class Game {
-
-    var players = [Player]()
+    
+    let playerManager:PlayerManager = PlayerManager()
+    let nameManager:NameManager = NameManager()
 
     func play(){
         print("Hello, Adventurers ! Welcome to OC RPG ! \n OC RPG is a simple battle game were you fight each other to death. Create your own team of heroes and defeat your opponent bravely ! \n\n")
         
-        players[0] = Player(name: askName(askPhrase: "So, what's your name Player 1 ?"))
+        playerManager.addPlayer(name: askName(askPhrase: "So, what's your name Player 1 ?"))
         
         print("Now, for an epic battle, you need a great opponent.\n")
         
-        players[1] = Player(name: askName(askPhrase: "How should I call you, Player 2 ?"))
+        playerManager.addPlayer(name: askName(askPhrase: "How should I call you, Player 2 ?"))
         
         print("\nAwesome ! Now that you are both here, why don't you take some time to build a team ? \n")
         print("You have to select 3 characters. I will ask you what class you want to pick and what is your character's name. When we're done with you \(players[0].name), it will be \(players[1].name)'s turn. Let's start ! \n")
         
         //PLAYER 1 TEAM SELECTION
-        players[0].team.append(createCharacter())
+        playerManager.getPlayer1().team.append(createCharacter())
         print("\nNow it's time to pick your second character.\n")
-        players[0].team.append(createCharacter())
+        playerManager.getPlayer1().team.append(createCharacter())
         print("\nLet's go for the last one !\n")
-        players[0].team.append(createCharacter())
+        playerManager.getPlayer1().team.append(createCharacter())
         
         //PLAYER 2 TEAM SELECTION
         print("\nPerfect, now it's your turn \(players[1].name).\n")
-        players[1].team.append(createCharacter())
+        playerManager.getPlayer2().team.append(createCharacter())
         print("\nNow it's time to pick your second character.\n")
-        players[1].team.append(createCharacter())
+        playerManager.getPlayer2().team.append(createCharacter())
         print("\nLet's go for the last one !\n")
-        players[1].team.append(createCharacter())
+        playerManager.getPlayer2().team.append(createCharacter())
         
         //RECAP
         print("\nNow it's time for a quick recap. Here's what your team are made of.\n")
-        print("\(players[0].name), you have : \n\(players[0].printTeam())\n")
-        print("\(players[1].name), you have : \n\(players[1].printTeam())\n")
+        print("\(playerManager.getPlayer1().name), you have : \n\(playerManager.getPlayer1().printTeam())\n")
+        print("\(playerManager.getPlayer2().name), you have : \n\(playerManager.getPlayer2().printTeam())\n")
         
         print("\nOk. Here's a quick reminder for the rules. Each of you will use his team to defeat his oppononent. The first player that takes down the whole team of his enemy will be the winner. Hope you guys are ready, cause here comes the battle !\n")
         
-        print("We will do a Heads or Tails to know who should start. If it's heads, \(players[0].name) starts. If it's tails, \(players[1].name) will start. Ready ? Here comes the throw... And...\n\n")
+        print("We will do a Heads or Tails to know who should start. If it's heads, \(playerManager.getPlayer1().name) starts. If it's tails, \(playerManager.getPlayer2().name) will start. Ready ? Here comes the throw... And...\n\n")
         
         //Create a fight object. Methods for the fight will be called from here
         let fight = Fight()
         
         //fight.beginNumber will do the heads or tails. It is a random value, 0 is player1 and 1 is player2.
         if(fight.beginNumber == 0){
-            print("Heads ! \(players[0].name), you can begin the fight !")
+            print("Heads ! \(playerManager.getPlayer1().name), you can begin the fight !")
         }
         else {
-            print("Tails ! \(players[1].name), you can begin the fight !")
+            print("Tails ! \(playerManager.getPlayer2().name), you can begin the fight !")
         }
     }
     
--- OC_RPG/OC_RPG/PlayerManager.swift
+++ OC_RPG/OC_RPG/PlayerManager.swift
@@ -10,4 +10,21 @@ import Foundation
 
 class PlayerManager {
     
+    //Stores the players of the game
+    var players = [Player]()
+    
+    //func to add players in the players array
+    func addPlayer(name:String) {
+        players.append(Player(name: name))
+    }
+    
+    //func to access player1 data
+    func getPlayer1() -> Player {
+        return players[0]
+    }
+    
+    //func to access player2 data
+    func getPlayer2() -> Player {
+        return players[1]
+    }
 }
